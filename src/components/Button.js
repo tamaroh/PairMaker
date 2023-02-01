4d561783ca87c670593a5772c8c71be99a1dd2ae
miku <svartkatt39@gmail.com>
2023-02-01 07:18:29 +0100

    feat: be able to update a cell in spread sheet

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,10 +6,10 @@ const Button = (props) => {
 
   async function go() {
     let array = [];
-    let pairStateCopy = []; 
+    let pairStateCopy = [];
 
     //配列をシャッフルする関数
-    function shuffle(){
+    function shuffle() {
       // console.log(state);
       for (let i = state.length - 1; i >= 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -18,37 +18,35 @@ const Button = (props) => {
       setDisplayState(true);
       // console.log(state);
       return state;
-    }; 
+    }
 
     //シャッフルしてできた配列をペアごとに分けて新しく用意した配列に追加
-    function pairSet(){
-        while (state.length > 1) {
-          array.push(state.splice(0, 2));
-        }
-        if (state.length === 1) {
-          array.push([state.shift()]);
-        }
-        //コピーした空の配列
-        // let pairStateCopy = pairState.slice(0);
-        pairStateCopy = pairState.slice(0);
+    function pairSet() {
+      while (state.length > 1) {
+        array.push(state.splice(0, 2));
+      }
+      if (state.length === 1) {
+        array.push([state.shift()]);
+      }
+      //コピーした空の配列
+      pairStateCopy = pairState.slice(0);
 
-        //取得した値をコピーした空の配列に追加
-        array.map((pair) => {
-          pairStateCopy.push(pair.join(" & "));
-        });
-        setPairState(pairStateCopy);
-        // console.log(pairStateCopy);
-        return pairState;
-    }; 
+      //取得した値をコピーした空の配列に追加
+      array.map((pair) => {
+        pairStateCopy.push(pair.join(" & "));
+      });
+      setPairState(pairStateCopy);
+      // console.log(pairStateCopy);
+      return pairState;
+    }
 
-    shuffle(); 
-    pairSet(); 
+    shuffle();
+    pairSet();
 
-    console.log("pairStatecopy", pairStateCopy); 
+    console.log("pairStatecopy", pairStateCopy);
 
-    const { data } = await axios.post("/gcp", { input : pairState });
-    console.log(data)
-
+    const { data } = await axios.post("/gcp", { input : JSON.stringify(pairState) });
+    console.log(data);
   }
 
   return (
