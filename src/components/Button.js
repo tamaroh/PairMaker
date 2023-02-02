b4ef39243e0e308261a10a5496a5d29f38e35eb4
Motoki-tech <motokimizuno.0107@gmail.com>
2023-02-02 17:31:20 +0900

    add:全てのペアパターン機能作成

--- src/components/Button.js
+++ src/components/Button.js
@@ -8,56 +8,99 @@ const Button = (props) => {
     let array = [];
     let pairStateCopy = [];
 
-    //配列をシャッフルする関数
+    //全体の組み合わせを作成
+    //もしペアが選ばれたときに、重複したペアパターンも削除
+    //指定した回数だけloopする
+    //もし奇数だった場合、ペアで選ばれなかった人を探してその人を最後のペアに追加する
+
+    const stateCopy = state;
+    let pairSet = [];
+    let result2;
     function shuffle() {
       if (state.length === 0) {
         return state;
       }
-      for (let i = state.length - 1; i >= 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [state[i], state[j]] = [state[j], state[i]];
+      let result = [];
+      for (let i = 0; i < state.length; i++) {
+        for (let j = stateCopy.length - 1; j >= 0; j--) {
+          result.push(state[i], stateCopy[j]);
+        }
       }
-      setDisplayState(true);
-      console.log(state);
-      return state;
+      while (result.length > 1) {
+        result2 = result.splice(0, 2).sort();
+        pairSet.push(result2);
+      }
+      console.log(pairSet);
     }
 
-    //シャッフルしてできた配列をペアごとに分けて新しく用意した配列に追加
-    function pairSet() {
-      while (state.length > 1) {
-        array.push(state.splice(0, 2));
-      }
-      // トータル人数が奇数だった場合に奇数人数のペアも作成
-      if (state.length === 1) {
-        array.push([state.shift()]);
-        let lastArray = array.pop();
-        array[array.length - 1].push(lastArray);
-      }
-
-      //コピーした空の配列
-      pairStateCopy = pairState.slice(0);
-
-      console.log(array);
-      //取得した値をコピーした空の配列に追加
-      // if (pairStateCopy.length % 2 === 1) {
-      //   pairStateCopy[pairStateCopy.length - 2].push(
-      //     pairStateCopy[pairStateCopy.length - 1].join(" & ")
-      //   );
-      // }
-      array.map((pair) => {
-        pairStateCopy.push(pair.join(" & "));
-      });
-      setPairState(pairStateCopy);
-      // console.log(pairStateCopy);
-      return pairState;
+    function deleateSamepair() {
+      const newArray = [];
+      pairSet
+        .filter((elem) => {
+          return elem[0] !== elem[1];
+        })
+        .forEach((elem) => {
+          if (!newArray.includes(String(elem))) {
+            newArray.push(String(elem));
+          }
+        });
+      console.log(newArray);
     }
 
     shuffle();
-    pairSet();
+    deleateSamepair();
+    // //配列をシャッフルする関数
+    // function shuffle() {
+    //   if (state.length === 0) {
+    //     return state;
+    //   }
+    //   for (let i = state.length - 1; i >= 0; i--) {
+    //     const j = Math.floor(Math.random() * (i + 1));
+    //     [state[i], state[j]] = [state[j], state[i]];
+    //   }
+    //   setDisplayState(true);
+    //   console.log(state);
+    //   return state;
+    // }
 
-    console.log("pairStatecopy", pairStateCopy);
+    // //シャッフルしてできた配列をペアごとに分けて新しく用意した配列に追加
+    // function pairSet() {
+    //   while (state.length > 1) {
+    //     array.push(state.splice(0, 2));
+    //   }
+    //   // トータル人数が奇数だった場合に奇数人数のペアも作成
+    //   if (state.length === 1) {
+    //     // debugger;
+    //     array.push(state);
+    //     let lastArray = String(array.pop());
+    //     array[array.length - 1].push(lastArray);
+    //   }
 
-    const { data } = await axios.post("/gcp", { input : JSON.stringify(pairState) });
+    //   //コピーした空の配列
+    //   pairStateCopy = pairState.slice(0);
+
+    //   console.log(array);
+    //   //取得した値をコピーした空の配列に追加
+    //   // if (pairStateCopy.length % 2 === 1) {
+    //   //   pairStateCopy[pairStateCopy.length - 2].push(
+    //   //     pairStateCopy[pairStateCopy.length - 1].join(" & ")
+    //   //   );
+    //   // }
+    //   array.map((pair) => {
+    //     pairStateCopy.push(pair.join(" , "));
+    //   });
+    //   setPairState(pairStateCopy);
+    //   return pairState;
+    // }
+
+    // shuffle();
+    // pairSet();
+
+    // console.log("pairStatecopy", pairStateCopy);
+
+    const { data } = await axios.post("/gcp", {
+      input: JSON.stringify(pairState),
+    });
     console.log(data);
   }
 
