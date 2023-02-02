813e700a5205ab22af9eb6d654360b3a88d12f23
Tamaroh <tamaroh@gmail.com>
2023-02-02 17:35:01 +0900

    worked on console only, pairs are in each array

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,5 @@
 import React from "react";
+import MakePair from "./MakePair";
 
 const Button = (props) => {
   let { students, setCount, pairs, setPairs } = props;
@@ -6,54 +7,57 @@ const Button = (props) => {
   //配列をシャッフルする関数
 
   function go() {
-    let array = [];
-    function shuffle() {
-      if (students.length === 0) {
-        return students;
-      }
-      for (let i = students.length - 1; i >= 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [students[i], students[j]] = [students[j], students[i]];
-      }
-      setCount(true);
-      console.log(students);
-      return students;
-    }
+    const result = MakePair(20, students);
+    setPairs(result);
+    console.log(result);
+    // let array = [];
+    // function shuffle() {
+    //   if (students.length === 0) {
+    //     return students;
+    //   }
+    //   for (let i = students.length - 1; i >= 0; i--) {
+    //     const j = Math.floor(Math.random() * (i + 1));
+    //     [students[i], students[j]] = [students[j], students[i]];
+    //   }
+    //   setCount(true);
+    //   console.log(students);
+    //   return students;
+    // }
 
-    //シャッフルしてできた配列をペアごとに分けて新しく用意した配列に追加
-    function pairSet() {
-      while (students.length > 1) {
-        array.push(students.splice(0, 2));
-      }
-      // トータル人数が奇数だった場合に奇数人数のペアも作成
-      if (students.length === 1) {
-        array.push([students.shift()]);
-        let lastArray = array.pop();
-        array[array.length - 1].push(lastArray);
-      }
+    // //シャッフルしてできた配列をペアごとに分けて新しく用意した配列に追加
+    // function pairSet() {
+    //   while (students.length > 1) {
+    //     array.push(students.splice(0, 2));
+    //   }
+    //   // トータル人数が奇数だった場合に奇数人数のペアも作成
+    //   if (students.length === 1) {
+    //     array.push([students.shift()]);
+    //     let lastArray = array.pop();
+    //     array[array.length - 1].push(lastArray);
+    //   }
 
-      //コピーした空の配列
-      let pairsCopy = pairs.slice(0);
+    //   //コピーした空の配列
+    //   let pairsCopy = pairs.slice(0);
 
-      console.log(array);
-      //取得した値をコピーした空の配列に追加
-      // if (pairsCopy.length % 2 === 1) {
-      //   pairsCopy[pairsCopy.length - 2].push(
-      //     pairsCopy[pairsCopy.length - 1].join(" & ")
-      //   );
-      // }
-      array.forEach((pair) => {
-        pairsCopy.push(pair.join(" & "));
-      });
+    //   console.log(array);
+    //   //取得した値をコピーした空の配列に追加
+    //   // if (pairsCopy.length % 2 === 1) {
+    //   //   pairsCopy[pairsCopy.length - 2].push(
+    //   //     pairsCopy[pairsCopy.length - 1].join(" & ")
+    //   //   );
+    //   // }
+    //   array.forEach((pair) => {
+    //     pairsCopy.push(pair.join(" & "));
+    //   });
 
-      setPairs(pairsCopy);
-      console.log(pairsCopy);
+    //   setPairs(pairsCopy);
+    //   console.log(pairsCopy);
 
-      return pairs;
-    }
+    //   return pairs;
+    // }
 
-    shuffle();
-    pairSet();
+    // shuffle();
+    // pairSet();
   }
 
   return (
