c418073a708c7fae3e2c5171120a0df6ecff1b6b
miku <svartkatt39@gmail.com>
2023-02-03 08:51:08 +0100

    add: implementing login

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-import * as dotenv from "dotenv"; 
-dotenv.config();
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import env from "react-dotenv"; 
+
+console.log(document.getElementById("root"));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <GoogleOAuthProvider clientId={process.env.GOOGLE_AUTH_CLIENT_ID}>
+  <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_AUTH_CLIENT_ID}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
