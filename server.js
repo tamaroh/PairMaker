5e59a21e71dc70522416ed5981983ed7fb70950f
miku <svartkatt39@gmail.com>
2023-02-07 17:58:29 +0100

    feat: update spread sheet

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const express = require("express");
-const doc = require("./google_spreadsheet_auth")
+const doc = require("./google_spreadsheet_auth");
 
 const app = express();
 app.use(express.json());
@@ -14,7 +14,7 @@ app.post("/gcp", async (req, res) => {
 
   let data = await req.body;
   data = JSON.parse(data.input);
-  // console.log("data: ", data)
+  console.log("data: ", data)
 
   await sheet.loadCells("A1:U21"); //セルの操作（読み込み）, @miku 20日分以上の組み合わせを作成または20組以上のペアを作成する場合は変更が必要
   let day_offset = 1; //　行のタイトル分のオフセット
@@ -22,18 +22,24 @@ app.post("/gcp", async (req, res) => {
   let counter = 0;
   //20日分ペアを組む設定　※定値
   for (
-    let day_index = 0 + day_offset; day_index < data.length + day_offset; day_index++
+    let day_index = 0 + day_offset;
+    day_index < data.length + day_offset;
+    day_index++
   ) {
-      for (
-        let pair_index = 0 + pair_offset; pair_index < data[day_index-1].length + pair_offset;
-        pair_index++
-        ) {
-      const cell = await sheet.getCell(day_index, pair_index);
-
-      cell.value = data[day_index-1][pair_index-1];
-      await sheet.saveUpdatedCells();
-      counter++;
-    }
+    console.log("day_index ", day_index); 
+    await data[day_index - 1].forEach((_, index) => {
+      return new Promise((resolve) => {
+          const cell = sheet.getCell(day_index, index + 1);
+          // console.log("cell 1: ", cell);
+          return resolve(cell);
+      }).then((cell) => {
+        setTimeout(() => {
+          console.log("cell 2: ", cell);
+          cell.value = data[day_index - 1][index];
+          sheet.saveUpdatedCells();
+        }, 5000)
+      });
+    });
   }
   res.send("update spread sheet!");
 });
