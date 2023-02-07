fe938ef2e4e5d1054c23e1d4c8ae2966087dd29f
miku <svartkatt39@gmail.com>
2023-02-07 15:28:45 +0100

    refactoring server

--- server.js
+++ server.js
@@ -14,25 +14,29 @@ app.post("/gcp", async (req, res) => {
 
   let data = await req.body;
   data = JSON.parse(data.input);
+  console.log("data: ", data)
 
-  await sheet.loadCells("A1:F6"); //セルの操作（読み込み）
+  await sheet.loadCells("A1:U21"); //セルの操作（読み込み）, @miku 20日分以上の組み合わせを作成または20組以上のペアを作成する場合は変更が必要
   let day_offset = 1; //　行のタイトル分のオフセット
   let pair_offset = 1; // 列のタイトル分のオフセット
   let counter = 0;
-  //現在は1日分だけペアを組む設定　※後で変更必要
+  //20日分ペアを組む設定　※定値
+  console.log("data: ", data)
   for (
-    let day_index = 0 + day_offset;
-    day_index < 1 + day_offset;
-    day_index++
-  ) {
-    for (
-      let pair_index = 0 + pair_offset;
-      pair_index < data.length + pair_offset;
-      pair_index++
+    // let day_index = 0 + day_offset;
+    // day_index < 1 + day_offset;
+    // day_index++
+    let day_index = 0 + day_offset; day_index < data.length + day_offset; day_index++
     ) {
+      for (
+        let pair_index = 0 + pair_offset;
+        pair_index < data.length + pair_offset;
+        pair_index++
+        ) {
+      console.log("data[pair_index]: ", data[pair_index -1]);
       const cell = await sheet.getCell(day_index, pair_index);
 
-      cell.value = data[counter];
+      cell.value = data[pair_index-1][counter];
       await sheet.saveUpdatedCells();
       counter++;
     }
