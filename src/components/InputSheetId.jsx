4b68b256adeb1681d01f7393c3fb3b105f5ef52f
miku <svartkatt39@gmail.com>
2023-02-23 10:30:00 +0100

    refactor: add styles

--- src/components/InputSheetId.jsx
+++ src/components/InputSheetId.jsx
@@ -5,7 +5,7 @@ import Form from "react-bootstrap/Form";
 const InputSheetId = (props) => {
   return (
     <>
-      <Form.Group className="mb-3 w-10 mt-5" controlId="exampleForm.ControlTextarea1">
+      <Form.Group className="mb-3 w-50 mt-5" controlId="exampleForm.ControlTextarea1">
         <Form.Control as="textarea" rows={1} onChange={(e) => {
           //テキストエリアのsheetIDを取得
           props.setSheetId(e.target.value);
