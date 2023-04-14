53521d8e4e5f53b9f5dc5786588c3ec454638697
miku <svartkatt39@gmail.com>
2023-04-14 12:34:03 +0200

    can start test

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,23 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+// import App from "./App";
+import Btn from "./components/Btn.jsx";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test("renders learn react link", () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+describe("Btn", () => {
+  test("renders Btn component", () => {
+    render(<Btn />);
+    const linkElement = screen.getByText(/make pairs/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
 
-//test
+// test("Pairs are made without omission in the number of people.", () => {
+//     render(<MakePair />);
+//     const linkElement = screen.getByText(/learn react/i);
+//     expect(linkElement).toBeInTheDocument();
+//   });
