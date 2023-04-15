d82c0ed9e016d3cc668a152cd5c6438252d1535a
miku <svartkatt39@gmail.com>
2023-04-15 12:24:20 +0200

    update test functions

--- testMakePair/test_utility.js
+++ testMakePair/test_utility.js
@@ -15,30 +15,80 @@ const test = (expected, actual) => {
  * @param {list} string - Two-dimensional-array
  * @return {object} object
  */
-
-const makeList = (list) => {
+const countPairMember = (list) => {
+  // console.log("list: ", list);
   let nameList = {};
-  list.forEach((element) => {
-    let nameCotainer = element.map((e) => {
-      return e.split("&");
-    });
-    nameCotainer.forEach((element) => {
-      element.forEach((elem, i) => {
-        for (let i = 0; i < element.length; i++) {
-          if (element[i] !== elem && nameList[elem] === undefined) {
-            nameList[elem] = {};
+  let container = [];
+  for (let i = 0; i < list.length; i++) {
+    let group = [];
+    for (let k = 0; k < list[i].length; k++) {
+      // console.log("list[i][k]", list[i][k].split("&"));
+      group.push(list[i][k].split("&"));
+    }
+    container.push(group); //コンテナには各日毎の配列にペアが配列で格納される
+  }
+  // console.log("container: ", container)
+
+  //コンテナから各メンバー毎にペアとなったメンバーとその回数をオブジェクトで記録する
+  for (let i = 0; i < container.length; i++) {
+    // console.log("container[i]: ", container[i])
+    let dailyList = container[i]; 
+    for (let k = 0; k < dailyList.length; k++) {
+      // console.log("group: dailyList[k]: ", dailyList[k]);
+      let dailyPair = dailyList[k]; 
+
+      dailyPair = dailyPair.map(e=>e.trim()); //ペアの配列の各要素から不要なスペースを削除しておく
+      for (let j = 0; j < dailyPair.length; j++) {
+        if (nameList[dailyPair[j]] === undefined) {
+          nameList[dailyPair[j]] = {};
+        }
+        for (let l = 0; l < dailyPair.length; l++) {
+          // console.log("dailyPair[l]: ", dailyPair[l]);
+          if (
+            dailyPair[j] !== dailyPair[l] &&
+            nameList[dailyPair[j]][dailyPair[l]] ===
+              undefined
+          ) {
+            nameList[dailyPair[j]][dailyPair[l]] = 0;
           }
-          if (element[i] !== elem && nameList[elem][element[i]] === undefined) {
-            nameList[elem][element[i]] = 0;
-          }
-          if (element[i] !== elem) {
-            nameList[elem][element[i]] += 1;
+          if (dailyPair[j] !== dailyPair[l]) {
+            nameList[dailyPair[j]][dailyPair[l]] += 1;
           }
         }
-      });
-    });
-  });
+      }
+    }
+  }
+  // console.log("nameList: ", nameList);
   return nameList;
 };
 
-module.exports = { test, makeList };
+/**
+ * あるメンバーが他のメンバーと3回より多くペアを組んでいないかをチェックする
+ *
+ * @param {list} object - countPairMemberの返り値であるペアのリスト
+ * @return {boolean} boolean 
+ */
+
+function checkPairs(list) {
+  let isValid = true;
+
+  const student = Object.keys(list);
+  const pairs = Object.values(list);
+
+  for (let i = 0; i < pairs.length; i++) {
+    // console.log(`${student[i]}: ${Object.values(pairs[i])}`);
+
+    if (
+      Object.values(pairs[i]).length !==
+      Object.values(pairs[i]).filter((e) => e < 4).length
+    ) {
+      console.log(`${student[i]} has paired with someone more than 3`);
+      console.log(`---`);
+      isValid = false;
+    }
+  }
+  return isValid;
+}
+
+
+module.exports = { test, countPairMember, checkPairs };
